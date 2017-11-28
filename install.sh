e5d6fbc19e97f05da01ab43ad4af2a70618ee9b4
thm <thm@rovsing.dk>
2017-11-28 23:43:50 +0100

    This is not a message (lazy commit)

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+
+mv PowerlineSymbols.otf /usr/share/fonts/
+
+fc-cache -vf /usr/share/fonts/
+mv 10-powerline-symbols.conf /etc/fonts/conf.d/
--- testScript.rb
+++
@@ -1,8 +0,0 @@
-#   Auther  : thm
-#   Date    : 22-November-2017
-#   File    : testScript.rb
-#   Project : scripts
-
-puts "Enter value"
-print "3000\r"
-gets
