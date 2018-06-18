a8832cf74d06abec585e510e65b194579132555d
thor <send.tilmig@gmail.com>
2018-06-18 13:15:35 +0200

    This is not a message (lazy commit)

--- bash_helper_functions.sh
+++ bash_helper_functions.sh
@@ -6,7 +6,7 @@ function checkAndSetIP_arg_masc(){
     if [ $# -ne 0 ]; then
         if [[ "$1" =~ ^[0-9]+$ ]] && [ "$1" -ge 0 -a "$1" -le 255 ]; then
         echo "New active device is $1";
-        ruby ip_parser.rb masc setActive $1
+        ruby ip_parser.rb $2 setActive $1
         else
         echo "Not a valid ip number [$1]. Must be 0 - 255"
         exit
