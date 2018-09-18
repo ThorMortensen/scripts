54fd53600cc79a1a4080017d1c557d9115b832da
thor <send.tilmig@gmail.com>
2018-09-19 00:48:53 +0200

    This is not a message (lazy commit)

--- netScanner.rb
+++ netScanner.rb
@@ -4,10 +4,9 @@ require 'differ'
 require_relative 'rubyHelpers.rb'
 
 
-cmd = TTY::Command.new(printer: :null)
+@cmd = TTY::Command.new(printer: :null)
 
-addr_infos = Socket.ip_address_list
-spinner    = TTY::Spinner.new(":spinner".blue + " Scanning net...".brown , format: :bouncing_ball)
+@addr_infos = Socket.ip_address_list
 
 
 
@@ -17,17 +16,35 @@ spinner    = TTY::Spinner.new(":spinner".blue + " Scanning net...".brown , forma
 
 # File.open(netScanDump, 'w') { |file| file.write("your text") }
 
-addr_infos.each do |ip|
-  next if ip.ip_address == "127.0.0.1"
-  if range = ip.ip_address.match(/(\d{1,3}\.\d{1,3}\.\d{1,3}\.)\d{1,3}/)
-    spinner.auto_spin
-    out, err = cmd.run("nmap -sP #{range[1]+'0'}/24")
-    spinner.stop('Done!'.green)
-    puts out
-    # (\d{1,3}\.\d{1,3}\.\d{1,3}\.)\d{1,3}
-  end 
-end
 
+def scanIp(ipAddress)
+  @spinner    = TTY::Spinner.new("Scanning with \'nmap -sP #{ipAddress}/24\' ".brown + ":spinner".blue, format: :bouncing_ball)
+  @spinner.auto_spin
+  out, err = @cmd.run("nmap -sP #{ipAddress}/24")
+  @spinner.stop('Done!'.green)
+  puts out
+
+end 
+
+
+
+if ARGV[1].nil?
+    @addr_infos.each do |ip|
+    next if ip.ip_address == "127.0.0.1"
+    if range = ip.ip_address.match(/(\d{1,3}\.\d{1,3}\.\d{1,3}\.)\d{1,3}/)
+      scanIp(range[1]+'0')
+    # (\d{1,3}\.\d{1,3}\.\d{1,3}\.)\d{1,3}
+    end 
+  end
+else 
+
+  if ARGV[1].match(/(\d{1,3}\.\d{1,3}\.\d{1,3}\.)\d{1,3}/)
+    scanIp(ARGV[1])
+  else 
+    puts "Not a valid ip!"
+  end 
+
+end 
 
 # cmd.run('ls -la')
 
